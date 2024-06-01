ad37ea03cd359477939d0caf5bf4ded5be244fa9
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-06-01 17:25:10 +0200

    feat(common): rethink api

--- rens-common/src/lib.rs
+++ rens-common/src/lib.rs
@@ -1,88 +1,17 @@
-/* Clippy config */
-#![allow(clippy::pattern_type_mismatch)]
 /* Modules */
+mod file;
 pub mod traits;
-mod file_name;
-/* Built-in imports */
-use core::num::ParseIntError;
 /* Dependencies */
-use derive_more::{Constructor, Display, FromStr, IsVariant};
+use derive_more::{Constructor, Display};
 use regex::Regex;
 /* Re-exports */
-pub use file_name::{FileName, RenameTarget};
+pub use file::{File, RenameTarget};
 
 #[derive(Debug, Display, Constructor)]
-#[display("{pattern}\n{with}\n{occurences}")]
-pub struct Renamer {
+#[display("{pattern}\n{with}\n{limit}")]
+pub struct Strategy {
     pattern: Regex,
     with: String,
-    occurences: Occurence,
-    target: RenameTarget,
-}
-
-impl Renamer {
-    #[inline]
-    #[must_use]
-    pub fn to_renamed_file(&self, file: &FileName) -> FileName {
-        let num = self.occurences.get_number_of_replacements();
-
-        match self.target {
-            RenameTarget::Both => {
-                let file_name = file.to_string();
-                let renamed =
-                    self.pattern.replacen(&file_name, num, &self.with);
-                match renamed.rsplit_once('.') {
-                    Some((stem, ext)) => {
-                        FileName::new(stem.to_owned(), Some(ext.to_owned()))
-                    },
-                    None => FileName::new(renamed.to_string(), None),
-                }
-            },
-            RenameTarget::Extension => FileName::new(
-                file.stem().clone(),
-                file.extension().clone().map(|ext| {
-                    self.pattern.replacen(&ext, num, &self.with).to_string()
-                }),
-            ),
-            RenameTarget::Stem => FileName::new(
-                self.pattern
-                    .replacen(file.stem(), num, &self.with)
-                    .to_string(),
-                file.extension().clone(),
-            ),
-        }
-    }
-}
-
-#[derive(Debug, Display, IsVariant, Clone)]
-#[allow(clippy::exhaustive_enums)]
-pub enum Occurence {
-    Global,
-    Numbered(usize),
-}
-
-impl Occurence {
-    /// Meant to be used with [`regex::Regex::renamen`] method.
-    /// In that method 0 replacements means all (global).
-    const fn get_number_of_replacements(&self) -> usize {
-        match *self {
-            Self::Global => 0,
-            Self::Numbered(num) => num,
-        }
-    }
-}
-
-impl FromStr for Occurence {
-    type Err = ParseIntError;
-
-    #[inline]
-    fn from_str(input: &str) -> Result<Self, Self::Err> {
-        match input {
-            "all" | "All" | "global" | "Global" | "g" => Ok(Self::Global),
-            str => match usize::from_str(str) {
-                Ok(count) => Ok(Self::Numbered(count)),
-                Err(err) => Err(err),
-            },
-        }
-    }
+    /// 0 means all
+    limit: usize,
 }
--- rens-common/src/traits/path_ext.rs
+++ rens-common/src/traits/path_ext.rs
@@ -1,15 +1,13 @@
-/* Clippy config */
-#![allow(clippy::pattern_type_mismatch)]
 /* Built-in imports */
 #[cfg(unix)]
 use std::os::unix::fs::FileTypeExt;
 #[cfg(windows)]
-use std::os::windows::fs::FileTypeExt;
+use std::os::windows::fs::{FileTypeExt, MetadataExt};
 use std::{fs, io, path::Path};
 /* Dependencies */
-use derive_more::{Display, IsVariant};
+use derive_more::Display;
 
-#[derive(Debug, Display, IsVariant)]
+#[derive(Debug, Display, PartialEq, Eq, Clone, Copy)]
 #[non_exhaustive]
 pub enum Kind {
     File,
@@ -20,11 +18,7 @@ pub enum Kind {
     Other,
 }
 
-pub trait PathExt {
-    fn kind(&self) -> io::Result<Kind>;
-}
-
-impl<P: AsRef<Path>> PathExt for P {
+pub trait PathExt: AsRef<Path> {
     #[inline]
     fn kind(&self) -> io::Result<Kind> {
         let metadata = fs::symlink_metadata(self)?;
@@ -50,4 +44,29 @@ impl<P: AsRef<Path>> PathExt for P {
 
         Ok(Kind::Other)
     }
+
+    #[inline]
+    #[cfg(unix)]
+    fn is_hidden(&self) -> bool {
+        use std::ffi::OsStr;
+
+        self.as_ref()
+            .file_name()
+            .map(OsStr::to_string_lossy)
+            .is_some_and(|name| name.starts_with('.'))
+    }
+
+    #[inline]
+    #[cfg(windows)]
+    fn is_hidden(&self) -> bool {
+        /// See: <https://learn.microsoft.com/en-us/windows/win32/fileio/file-attribute-constants>
+        const FILE_ATTRIBUTE_HIDDEN: u32 = 0x0000_0002;
+
+        self.as_ref()
+            .metadata()
+            .map(|metadata| metadata.file_attributes())
+            .is_ok_and(|attr| attr & FILE_ATTRIBUTE_HIDDEN != 0)
+    }
 }
+
+impl<P: AsRef<Path>> PathExt for P {}
